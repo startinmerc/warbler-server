aeb1db5737797a116f9c0cb55db9ad47d5931854
STM <startinmerc@gmail.com>
2020-01-06 16:50:59 +0000

    add ensureCorrectUser middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,5 +25,22 @@ exports.loginRequired = function(req,res,next){
 
 // make sure user is correct
 exports.ensureCorrectUser = function(req,res,next){
-
-}
+	try {
+		const token = req.headers.authorization.split(" ")[1];
+		jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
+			if(decoded && decoded.id === req.params.id) {
+				return next();
+			} else {
+				return next({
+					status: 401,
+					message: "Unauthorized"
+				})
+			}
+		})
+	} catch(err) {
+		return next({
+			status: 401,
+			message: "Unauthorized"
+		});
+	}
+};
