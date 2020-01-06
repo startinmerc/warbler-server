7b56b62f5ac4b7762291d372463b0a6dd4eab79e
STM <startinmerc@gmail.com>
2020-01-06 14:21:15 +0000

    mongoose config

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", {
+mongoose.connect("mongodb://localhost:27017/warbler", {
 	keepAlive: true,
-	useMongoClient: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
+
+module.exports.User = require("./user");
