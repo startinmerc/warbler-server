157fafb21d9424f88803575e4d76ab9c79db07c6
STM <startinmerc@gmail.com>
2020-01-06 16:50:33 +0000

    fix errors (mostly syntax)

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-require('dotenv').load();
+require('dotenv').config();
 const jwt = require("jsonwebtoken");
 
 // make sure user is logged in
 exports.loginRequired = function(req,res,next){
 	try {
-		const token = req.headers.authoriszation.split(" ")[1]
+		const token = req.headers.authorization.split(" ")[1];
 		jwt.verify(token, process.env.SECRET_KEY, function(err,decoded){
 			if(decoded){
 				return next();
