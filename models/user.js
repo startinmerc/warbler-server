f3b518877366362872964a7094e539af9f1bb5a1
STM <startinmerc@gmail.com>
2020-01-06 12:16:47 +0000

    add user middleware to hash password

--- models/user.js
+++ models/user.js
@@ -21,6 +21,18 @@ const userSchema = new mongoose.Schema({
 	}
 });
 
+userSchema.pre("save", async function(next){
+	try {
+		if(!this.isModified("password")){
+			return next();
+		}
+		let hashedPassword = await bcrypt.hash(this.password, 10);
+		this.password = hashedPassword;
+		return next();
+	} catch (err) {
+		return next(err);
+	}
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
