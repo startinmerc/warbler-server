2b3d07b4c3ddd94a65e7d7816857bd1703464973
STM <startinmerc@gmail.com>
2020-01-06 16:50:03 +0000

    add middleware to app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
 const messagesRoutes = require("./routes/messages");
-
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 const PORT = 8081;
 
 app.use(cors());
@@ -15,7 +15,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // routes
 app.use("/api/auth", authRoutes);
-app.use("/api/users/:id/messages", messagesRoutes);
+app.use("/api/users/:id/messages",
+	loginRequired,
+	ensureCorrectUser,
+	messagesRoutes
+);
 
 app.use(function(req,res,next){
 	let err = new Error("Not Found");
