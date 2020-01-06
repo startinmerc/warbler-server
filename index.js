03e3d5f6d023b8534e4403d7de0090a787c8d82c
STM <startinmerc@gmail.com>
2020-01-06 17:19:22 +0000

    require models in app

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/auth");
 const messagesRoutes = require("./routes/messages");
 const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 const PORT = 8081;
+const db = require("./models");
 
 app.use(cors());
 app.use(bodyParser.json());
